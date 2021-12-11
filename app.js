cc1ca77f48b51b30fcef670b69463d8d1f87023b
AlienDance <vlad0502vlader@gmail.com>
2021-12-11 17:49:11 +0200

    lorem generator added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { lorem, firstLetterCap } = require('./lorem-ipsum/lorem');
 app.listen(5000, () => console.log('server is up!'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
@@ -14,10 +16,16 @@ app.get('/landing', (req, res) => {
   res.render('landing', { title: 'SOMETHING' });
 });
 
-app.get('/lorem', (req, res) => {
-  res.json({
-    loremSmall: lorem.generateSentences(2),
-    loremBig: lorem.generateSentences(6),
-    loremTitle: firstLetterCap(lorem.generateWords(3))
-  });
+app.post('/lorem', (req, res) => {
+  if (req.body.sentQuan) {
+    res.json({
+      lorem: lorem.generateSentences(Number(req.body.sentQuan))
+    });
+  } else if (req.body.card) {
+    res.json({
+      loremSmall: lorem.generateSentences(2),
+      loremBig: lorem.generateSentences(6),
+      loremTitle: firstLetterCap(lorem.generateWords(3))
+    });
+  }
 });
